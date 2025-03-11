0e7304108332fa749fa6d420379128c51aa80f53
hyundo <hyundo13@naver.com>
2025-03-11 09:21:13 +0900

    SeasonCoffee 생성

--- Java/bc_coffeemanager/src/com/grepp/coffeemanager/domain/order/Order.java
+++ Java/bc_coffeemanager/src/com/grepp/coffeemanager/domain/order/Order.java
@@ -1,6 +1,7 @@
 package com.grepp.coffeemanager.domain.order;
 
 import com.grepp.coffeemanager.domain.coffee.Coffee;
+import com.grepp.coffeemanager.domain.coffee.SeasonCoffee;
 import java.time.LocalDateTime;
 
 public class Order {
@@ -16,6 +17,10 @@ public class Order {
 
     if(drink.getStock() < orderCnt){
       order.status = OrderStatus.FAIL_SOLDOUT;
+    } else if (drink instanceof SeasonCoffee seasonCoffee) {
+      if(seasonCoffee.notSeason()){
+        order.status = OrderStatus.FAIL_SEASON;
+      }
     } else {
       order.status = OrderStatus.OK;
     }
