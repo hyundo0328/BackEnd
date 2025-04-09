e8afc9e66c702fa5e20c10c489ed580769254cbf
hyundo <hyundo13@naver.com>
2025-04-09 10:03:17 +0900

    Servlet

---
+++ Servlet/a_servlet/src/main/java/com/grepp/servlet/app/member/MemberController.java
@@ -0,0 +1,80 @@
+
+package com.grepp.servlet.app.member;
+
+import com.grepp.servlet.app.auth.Principal;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/member/*")
+public class MemberController extends HttpServlet {
+
+  private final MemberService memberService = new MemberService();
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    String url = req.getRequestURI();
+    String[] urlArr = url.split("/");
+
+    switch (urlArr[urlArr.length-1]){
+      case "login":
+        page(req, resp);
+        break;
+      case "logout":
+        logout(req, resp);
+        break;
+      default:
+        resp.setStatus(404);
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String url = req.getRequestURI();
+    String[] urlArr = url.split("/");
+
+    switch (urlArr[urlArr.length-1]){
+      case "login":
+        login(req, resp);
+        break;
+      default:
+        resp.setStatus(404);
+    }
+  }
+
+  private void page(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    req.getRequestDispatcher("/WEB-INF/view/member/login.jsp")
+        .forward(req, resp);
+  }
+
+  private void logout(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    HttpSession session = req.getSession();
+    session.removeAttribute("principal");
+    resp.sendRedirect("/");
+  }
+
+  private void login(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    HttpSession session = req.getSession();
+    String userId = req.getParameter("userId");
+    String password = req.getParameter("password");
+
+    resp.setHeader("set-cookie", "JSESSIONID="
+        + session.getId()
+        + "; max-age=3600; path=/");
+
+    Principal principal = memberService.authenticate(userId, password);
+
+    session.setAttribute("principal", principal);
+    resp.sendRedirect("/");
+  }
+
+
+}
---
+++ Servlet/a_servlet/src/main/java/com/grepp/servlet/study/c_listener/A_listener.java
@@ -0,0 +1,43 @@
+package com.grepp.servlet.study.c_listener;
+
+import com.grepp.servlet.infra.db.JdbcTemplate;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpSessionAttributeListener;
+import jakarta.servlet.http.HttpSessionBindingEvent;
+
+@WebListener
+public class A_listener implements ServletContextListener, HttpSessionAttributeListener {
+    
+    private ServletContext context;
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        
+        String url = "jdbc:mysql://localhost:3306/jdbc?useUnicode=true&characterEncoding=utf8";
+        String user = "bm";
+        String password = "1q2w3e!@#";
+        JdbcTemplate.init(url, user, password);
+        
+        context = sce.getServletContext();
+        context.setAttribute("userCnt", 0);
+    }
+    
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent event) {
+        if(event.getName().equals("principal")){
+            int cnt = (int) context.getAttribute("userCnt");
+            context.setAttribute("userCnt", ++cnt);
+        }
+    }
+    
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent event) {
+        if(event.getName().equals("principal")){
+            int cnt = (int) context.getAttribute("userCnt");
+            context.setAttribute("userCnt", --cnt);
+        }
+    }
+}
